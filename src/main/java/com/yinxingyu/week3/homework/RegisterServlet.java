3315b1fb75f81e7839615cb3107549b983ed1512
殷星雨 <Yinxingyu0000@163.com>
2021-03-27 22:31:20 +0800

    4.HW-CE2 Commit

--- src/main/java/com/yinxingyu/week3/homework/RegisterServlet.java
+++ src/main/java/com/yinxingyu/week3/homework/RegisterServlet.java
@@ -2,12 +2,32 @@ package com.yinxingyu.week3.homework;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.*;
 
-@WebServlet(name = "RegisterServlet", value = "/RegisterServlet")
+@WebServlet(name = "RegisterServlet",value = "/register")
+
 public class RegisterServlet extends HttpServlet {
+
+    public Connection con;
+
+    public void init() throws ServletException{
+        String driver = getServletContext().getInitParameter("driver");
+        String url = getServletContext().getInitParameter("url");
+        String username = getServletContext().getInitParameter("username");
+        String password = getServletContext().getInitParameter("password");
+
+        try{
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -15,6 +35,8 @@ public class RegisterServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String id = request.getParameter("id");
         String Username = request.getParameter("Username");
         String Password = request.getParameter("Password");
         String Email = request.getParameter("Email");
@@ -22,14 +44,54 @@ public class RegisterServlet extends HttpServlet {
         String BirthDate = request.getParameter("BirthDate");
 
         PrintWriter writer = response.getWriter();
-        writer.println("<br/>");
-        writer.println("<br/>Username: " + Username);
-        writer.println("<br/>Password: " + Password);
-        writer.println("<br/>Email: " + Email);
-        writer.println("<br/>Gender: " + Gender);
-        writer.println("<br/>BirthDate: "  + BirthDate);
-        writer.println("<br/>");
+        try {
 
-        writer.close();
+            System.out.println("con:"+con);
+            Statement createDbStatement = con.createStatement();
+            String insertDb = "insert into usertable(Username,Password,Email,Gender,BirthDate) values('"+Username+"','"+Password+"','"+Email+"','"+Gender+"','"+BirthDate+"')";
+            createDbStatement.executeUpdate(insertDb);
+            String selectDb = "select * from usertable";
+            ResultSet rs = createDbStatement.executeQuery(selectDb);
+            while(rs.next()) {
+                rs.getString("id");
+                rs.getString("UserName");
+                rs.getString("Password");
+                rs.getString("Email");
+                rs.getString("Gender");
+                rs.getString("BirthDate");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        writer.println(
+                "<table border=\"1\">"       +
+                    "<tr>"                   +
+                        "<td>id</td>"        +
+                        "<td>UserName</td>"  +
+                        "<td>Password</td>"  +
+                        "<td>Email</td>"     +
+                        "<td>Gender</td>"    +
+                        "<td>BirthDate</td>" +
+                    "</tr>"    +
+                    "<tr>"     +
+                        "<td>" + id        + "</td>" +
+                        "<td>" + Username  + "</td>" +
+                        "<td>" + Password  + "</td>" +
+                        "<td>" + Email     + "</td>" +
+                        "<td>" + Gender    + "</td>" +
+                        "<td>" + BirthDate + "</td>" +
+                    "</tr>"    +
+                "</table>");
     }
+//        writer.println("<br/>");
+//        writer.println("<br/>Username: " + Username);
+//        writer.println("<br/>Password: " + Password);
+//        writer.println("<br/>Email: " + Email);
+//        writer.println("<br/>Gender: " + Gender);
+//        writer.println("<br/>BirthDate: "  + BirthDate);
+//        writer.println("<br/>");
+//
+//        writer.close();
+
 }
