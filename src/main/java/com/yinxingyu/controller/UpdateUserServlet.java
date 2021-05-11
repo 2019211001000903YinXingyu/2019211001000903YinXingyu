03c00dbacf1c868f3130c9cc5777dc6f0dfcc878
yinXingyu <Yinxingyu0000@163.com>
2021-05-11 23:40:42 +0800

    9.HW-CE2 Commit

--- src/main/java/com/yinxingyu/controller/UpdateUserServlet.java
+++ src/main/java/com/yinxingyu/controller/UpdateUserServlet.java
@@ -3,9 +3,12 @@ package com.yinxingyu.controller;
 import com.yinxingyu.dao.UserDao;
 import com.yinxingyu.model.User;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -15,6 +18,13 @@ import java.util.Date;
 
 @WebServlet(name = "UpdateUserServlet", value = "/updateUser")
 public class UpdateUserServlet extends HttpServlet {
+
+    private Connection con;
+
+    @Override
+    public void init() throws ServletException {
+        con = (Connection) getServletContext().getAttribute("con");
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -33,31 +43,40 @@ public class UpdateUserServlet extends HttpServlet {
         // TODO 5 : call updateUser() of UserDao for update information
         // TODO 6 : forward to WEB-INF/views/UserInfo.jsp
 
-        String id = request.getParameter("id");
+        int id = Integer.parseInt(request.getParameter("id"));
         String Username = request.getParameter("Username");
         String Password = request.getParameter("Password");
         String Email = request.getParameter("Email");
         String Gender = request.getParameter("Gender");
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        Date BirthDate = null;
 
         try {
-            System.out.println(request.getParameter("BirthDate").trim());
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            Date BirthDate = sdf.parse(request.getParameter("BirthDate"));
 
-            BirthDate = simpleDateFormat.parse(request.getParameter("BirthDate").trim());
+            User user = new User();
+            user.setId(id);
+            user.setUsername(Username);
+            user.setPassword(Password);
+            user.setEmail(Email);
+            user.setGender(Gender);
+            user.setBirthDate(BirthDate);
+
+            UserDao userDao = new UserDao();
+            Connection con = (Connection) getServletContext().getAttribute("con");
+            try {
+                if (userDao.updateUser(con, user) != 0) {
+                    User u = userDao.findByUsernamePassword(con, Username, Password);
+                    HttpSession session = request.getSession();
+                    session.setMaxInactiveInterval(10);
+                    session.setAttribute("user", u);
+                    request.getRequestDispatcher("WEB-INF/views/UserInfo.jsp").forward(request, response);
+                }
+            } catch (SQLException throwable) {
+                throwable.printStackTrace();
+            }
+
         } catch (ParseException e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
-
-        User u = new User(Integer.parseInt(id), Username, Password, Email, Gender, BirthDate);
-        UserDao userDao = new UserDao();
-        try {
-            userDao.updateUser((Connection) getServletContext().getAttribute("con"), u);
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-        System.out.println(BirthDate);
-
-        request.getRequestDispatcher("WEB-INF/views/UserInfo.jsp").forward(request, response);
     }
 }
